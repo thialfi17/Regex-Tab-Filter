e715ce331c43ff1d59cd2f45bd69058ace407269
thialfi17 <thialfi17@gmail.com>
2020-03-09 22:45:13 +0000

    Added new menu item. Made animations appear faster.

--- background.js
+++ background.js
@@ -8,6 +8,27 @@ browser.menus.create({
     title: browser.i18n.getMessage("menuItemHideTab"),
     visible: false,
     contexts: ["tab"]
+}, function () {
+    browser.tabs.query({
+        highlighted: true
+    }).then((tabs) => {
+        if (tabs.length == 2) {
+            browser.menus.update(
+                "hide_tab",
+                {
+                    visible: true
+                }
+            );
+        } else if (tabs.length >= 3) {
+            browser.menus.update(
+                "hide_tab",
+                {
+                    visible: true,
+                    title: browser.i18n.getMessage("menuItemHideTabs")
+                }
+            );
+        }
+    });
 });
 
 // Add context menu item for hiding other tabs
@@ -16,6 +37,43 @@ browser.menus.create({
     title: browser.i18n.getMessage("menuItemHideOtherTabs"),
     visible: true,
     contexts: ["tab"]
+}, function() {
+    browser.tabs.query({
+        hidden: false,
+        highlighted: false
+    }).then((tabs) => {
+        if (tabs.length == 0) {
+            browser.menus.update(
+                "hide_other_tabs",
+                {
+                    visible: false
+                }
+            );
+        }
+    });
+});
+
+// Add menu item to show all hidden tabs. This is already easy to do with the
+// address bar but might as well add it to the context menu since the context
+// menu can already do so much.
+browser.menus.create({
+    id: "show_tabs",
+    title: browser.i18n.getMessage("menuItemShowTabs"),
+    visible: false,
+    contexts: ["tab"]
+}, function() {
+    browser.tabs.query({
+        hidden: true
+    }).then((tabs) => {
+        if (tabs.length > 1) {
+            browser.menus.update(
+                "show_tabs",
+                {
+                    visible: true
+                }
+            );
+        }
+    });
 });
 
 browser.menus.onClicked.addListener((info, tab) => {
@@ -24,20 +82,31 @@ browser.menus.onClicked.addListener((info, tab) => {
             browser.tabs.query({
                 highlighted: true
             }).then((tabs) => {
-                for (let tab of tabs) {
-                    browser.tabs.hide(tab.id);
+                browser.tabs.hide(getTabIds(tabs));
+            });
+
+            browser.menus.update(
+                "hide_tab",
+                {
+                    visible: false
                 }
-            })
+            );
             break;
         case "hide_other_tabs":
             browser.tabs.query({
                 highlighted: false,
                 currentWindow: true
             }).then((tabs) => {
-                for (let tab of tabs) {
-                    browser.tabs.hide(tab.id);
-                }
+                browser.tabs.hide(getTabIds(tabs));
             })
+            break;
+        case "show_tabs":
+            browser.tabs.query({
+                currentWindow: true,
+                hidden: true
+            }).then((tabs) => {
+                browser.tabs.show(getTabIds(tabs));
+            });
             break;
     }
 });
@@ -52,7 +121,7 @@ browser.tabs.onHighlighted.addListener((highlightInfo) => {
             }
         );
     // Show the menu item with the singular form when two tabs are highlighted
-    } else if (highlightInfo.tabId.length == 2) {
+    } else if (highlightInfo.tabIds.length == 2) {
         browser.menus.update(
             "hide_tab",
             {
@@ -61,14 +130,43 @@ browser.tabs.onHighlighted.addListener((highlightInfo) => {
             }
         );
     // Change the menu item to show the plural form when more than three tabs are highlighted
-    } else if (highlightInfo.tabIds.length == 3) {
+    } else if (highlightInfo.tabIds.length > 2) {
         browser.menus.update(
             "hide_tab",
             {
+                visible: true,
                 title: browser.i18n.getMessage("menuItemHideTabs")
             }
         );
     }
+
+    checkHideOtherTabs();
+});
+
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tabInfo) => {
+    browser.tabs.query({
+        hidden: true
+    }).then((tabs) => {
+        if (tabs.length > 0) {
+            browser.menus.update(
+                "show_tabs",
+                {
+                    visible: true
+                }
+            );
+        } else {
+            browser.menus.update(
+                "show_tabs",
+                {
+                    visible: false
+                }
+            );
+        }
+    });
+
+    checkHideOtherTabs();
+}, {
+    properties: ["hidden"]
 });
 
 browser.omnibox.onInputEntered.addListener((text, disposition) => {
@@ -100,19 +198,61 @@ browser.omnibox.onInputEntered.addListener((text, disposition) => {
     browser.tabs.query({
         currentWindow: true
     }).then((tabs) => {
+        matchIds = [];
+        nonMatchIds =[];
+
         for (let tab of tabs) {
             // Apply actions on tabs if they match
             if ((filter.test(tab.title) || filter.test(tab.url))) {
-                if (on_match_action == HIDE) {
-                    browser.tabs.hide(tab.id);
-                } else if (on_match_action == SHOW) {
-                    browser.tabs.show(tab.id);
-                }
+                matchIds.push(tab.id);
             }
             // Hide tabs that didn't match a condition when not inverted
-            else if (not_match_action == HIDE) {
-                browser.tabs.hide(tab.id);
+            else {
+                nonMatchIds.push(tab.id);
             }
+        }
+
+        if (on_match_action == HIDE) {
+            browser.tabs.hide(matchIds);
+        } else if (on_match_action == SHOW) {
+            browser.tabs.show(matchIds);
+        }
+
+        if (not_match_action == HIDE) {
+            browser.tabs.hide(nonMatchIds);
+        } else if (not_match_action == SHOW) {
+            browser.tabs.show(nonMatchIds);
         }
     });
 });
+
+function getTabIds(tabs) {
+    let tabIds = [];
+    for (let tab of tabs) {
+        tabIds.push(tab.id);
+    }
+    return tabIds;
+}
+
+function checkHideOtherTabs() {
+    browser.tabs.query({
+        hidden: false,
+        highlighted: false
+    }).then((tabs) => {
+        if (tabs.length > 0) {
+            browser.menus.update(
+                "hide_other_tabs",
+                {
+                    visible: true
+                }
+            );
+        } else {
+            browser.menus.update(
+                "hide_other_tabs",
+                {
+                    visible: false
+                }
+            );
+        }
+    });
+}
